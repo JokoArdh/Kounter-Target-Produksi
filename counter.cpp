838fdf726ba3974f81e843be4e03b7c4a6826abd
JokoArdh <c20010007@uby.ac.id>
2024-01-25 15:06:14 +0700

    update firmware versi 1

--- counter.cpp
+++ counter.cpp
@@ -1,45 +1,62 @@
-#include <RTClib.h>
-#include <Wire.h>
+#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <RTClib.h> //define rtclib 
+#include <SPI.h> //gatuane rtclib jadiwajib
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 RTC_DS3231 rtc;
-
-int proximityPin = D2; 
-int count = 0;
+//define pin sensor proximiy dan buzzer
+int proximityPin = D2;
+int buzzerPin = D0;
+float count = 0;
+//membuat interval dan parameter waktu
+const int interval = 1800000; // 30 menit dalam milisecond
+unsigned long before = 0;
+unsigned long lastRiset = 0;
 
 void setup() {
-  // put your setup code here, to run once
+  // put your setup code here, to run once:
   pinMode(proximityPin, INPUT);
-  // Wire.begin(5,4);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Status");
+  lcd.print("Status: ");
 
-  rtc.begin(); 
+  rtc.begin();
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  DateTime now = rtc.now();
   int value = digitalRead(proximityPin);
 
-  if(value == 0){ // jika daya low maka tidak terdeteksi
-    count++;
-  }
-   lcd.setCursor(0, 1);
-   lcd.print(count);
-   delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print(now.day());
+  lcd.print("/");
+  lcd.print(now.month());
+  lcd.print("/");
+  lcd.print(now.year());
 
-  DateTime now = rtc.now();
-  if(now.minute() == 2 && now.second() == 120){
-    count = 0;
-  }
-   lcd.setCursor(0, 1);
-   lcd.print(count);
-
-  
+if(value == 0){
+  count++;
+  digitalWrite(buzzerPin, HIGH);
+}else{
+  digitalWrite(buzzerPin, LOW);
 }
+//percabangan waktu untuk riset otomatis
+unsigned long waktuNow = millis();
+if (waktuNow - before >= interval)
+{ 
+ count = 0; //riset cont ke 0
+ lastRiset = waktuNow; //update riset time
+ before = waktuNow; //perbarui waktu terakhir u perhitungan berikutnya
+}
+lcd.setCursor(7, 0);
+lcd.print(count);
+delay(500);
 
-
+}
